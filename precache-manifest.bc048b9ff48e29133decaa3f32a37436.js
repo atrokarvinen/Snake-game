bbffb0f9eed4e7a07272d518b95c89c1f031f573
Atro Karvinen <atro.karvinen@roimaint.com>
2020-01-17 10:25:29 +0200

    Updates

---
+++ precache-manifest.bc048b9ff48e29133decaa3f32a37436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f4ba0c36a1c363abe77b95c229d471",
+    "url": "/Snake-game/index.html"
+  },
+  {
+    "revision": "09c3bc7c28058fe263ed",
+    "url": "/Snake-game/static/css/main.8f87e8da.chunk.css"
+  },
+  {
+    "revision": "b506c1fbf2cf39699fb1",
+    "url": "/Snake-game/static/js/2.044d0451.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Snake-game/static/js/2.044d0451.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09c3bc7c28058fe263ed",
+    "url": "/Snake-game/static/js/main.7dfcec7b.chunk.js"
+  },
+  {
+    "revision": "01b9bf96cd8386c35cf1",
+    "url": "/Snake-game/static/js/runtime-main.ce6c28ea.js"
+  }
+]);
